76dfce18b3dc374d04292c9562704d6fb81a7fdf
oksentiukpn <oksentiuk.pn@ucu.edu.ua>
2026-04-28 18:31:50 +0300

    create RDS, create table with some data, created lambda function to connect to RDS and fetch data

---
+++ db_test/hello_world/app.py
@@ -0,0 +1,60 @@
+import json
+from os import environ
+
+import boto3
+import pymysql
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+HOSTNAME = environ.get("HOSTNAME")
+USERNAME = environ.get("USERNAME")
+PASSWORD = environ.get("PASSWORD")
+
+
+def get_db_connection():
+    connection = pymysql.connect(
+        host=HOSTNAME,
+        user=USERNAME,
+        password=PASSWORD,
+        database="mydb",
+    )
+    return connection
+
+
+def lambda_handler(event, context):
+    """Sample pure Lambda function
+
+    Parameters
+    ----------
+    event: dict, required
+        API Gateway Lambda Proxy Input Format
+
+        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+
+    context: object, required
+        Lambda Context runtime methods and attributes
+
+        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
+
+    Returns
+    ------
+    API Gateway Lambda Proxy Output Format: dict
+
+        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+    """
+    connection = get_db_connection()
+
+    query = "SELECT * FROM users"
+    cursor = connection.cursor()
+    cursor.execute(query)
+
+    results = cursor.fetchall()
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(results),
+    }
+
+
+print(lambda_handler({}, {}))
